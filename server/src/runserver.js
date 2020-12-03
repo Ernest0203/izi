95291a19a43f1e2ff18183176ffe3e196b7efa36
Ernest0203 <ernest2332@gmail.com>
2020-12-03 14:58:40 +0200

    commit

--- server/src/runserver.js
+++ server/src/runserver.js
@@ -12,9 +12,9 @@ console.info('NODE_ENV', process.env.NODE_ENV);
 const HOST = config.get('server.host');
 const PORT = config.get('server.port');
 
-const instance = server.listen(PORT || 3000, '127.0.0.1');
+const instance = server.listen(5000, '127.0.0.1');
 
 instance.on('listening', () => console.info('Available on:', `${HOST}:${PORT}`));
 instance.on('error', (error) => console.error(error));
 
-exports.default = instance;
+module.exports = instance;
