1f3237d83df0cdaa9086b421cb1172bd59782f99
Ernest0203 <ernest2332@gmail.com>
2020-12-03 16:03:01 +0200

    commit

--- server/src/server.js
+++ server/src/server.js
@@ -14,10 +14,10 @@ app.use(express.static(path.join(__dirname, '../', 'resources/')));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
+  app.use(express.static(__dirname, '../', 'client', 'build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve()(__dirname, '../', '../', 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'));
   });
 }
 
