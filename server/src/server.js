3b397ae88b4588ceb3c614d4dbaec4f62c6eb4ce
Ernest0203 <ernest2332@gmail.com>
2020-12-03 16:10:31 +0200

    commit

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../', 'resources/')));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, '../', 'client', 'build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'));
