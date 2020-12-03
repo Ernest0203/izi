b90ae18cc8c950e4c03ff84935086272da3075af
Ernest0203 <ernest2332@gmail.com>
2020-12-03 16:29:32 +0200

    fix

--- README.md
+++ README.md
@@ -11,15 +11,3 @@ npm run install-all
 ```bash
 npm run dev
 ```
-
-## Deploy to heroku
-
-```bash
-heroku login
-heroku create
-git init
-heroku git:remote -a created-repo-name
-git add .
-git commit -am 'commit'
-git push heroku master
-```
--- server/src/runserver.js
+++ server/src/runserver.js
@@ -12,7 +12,7 @@ console.info('NODE_ENV', process.env.NODE_ENV);
 const HOST = config.get('server.host');
 const PORT = config.get('server.port');
 
-const instance = server.listen(process.env.PORT || 2000, '0.0.0.0');
+const instance = server.listen(PORT || HOST);
 
 instance.on('listening', () => console.info('Available on:', `${HOST}:${PORT}`));
 instance.on('error', (error) => console.error(error));
